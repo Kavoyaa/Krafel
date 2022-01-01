3c05775047c4c741dc3dfdcc2f0d939fbc2201a9
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2022-01-01 13:10:20 +0000

    New command: poll

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.start((ctx) => {
 
 // Help
 bot.help((ctx) => {
-	ctx.reply('Krafel Command List\n\nCurrent commands:\n-/start : Shows the introductory message.\n-/help : Shows a list of all commands(this message).\n-/hello : Says hello.\n-/bye : Says bye.\n-/say <msg> : Says the given meessage.')
+	ctx.replyWithMarkdown('*Krafel Command List*\n\nCurrent commands:\n-/start : Shows the introductory message.\n-/help : Shows a list of all commands(this message).\n-/hello : Says hello.\n-/bye : Says bye.\n-/say <message> : Says the given message.\n-/poll <question> | <option 1>, <option 2>, <option 3> : Makes a poll.')
 })
 
 // Hello
@@ -35,6 +35,21 @@ bot.command('say', (ctx) => {
 		ctx.reply(message)
 	} else {
 		ctx.replyWithMarkdown('You need to specify a message to say!\n\n*Example:*\n/say Hello!')
+	}
+})
+
+// Poll
+bot.command('poll', (ctx) => {
+	try {
+		let message = ctx.message['text']
+		let params = message.replace('/poll ', '')
+		params = params.split('|')
+		let question = params[0]
+		let options = params[1]
+		options = options.split(',')
+		ctx.replyWithPoll(question, options, { is_anonymous: false})
+	} catch (e) {
+		ctx.replyWithMarkdown('You need to follow the command syntax correctly!\n\n*Example:*\n/poll This is question | Option One, Option 2, Option 3\n\n*You must:*\n-Seprate the question and the options with a "|"(vertical slash).\n-Seprate the options with a ","(comma).\n-Specify atleast 2 options.\n\nYou can have upto 10 options.')
 	}
 })
 
