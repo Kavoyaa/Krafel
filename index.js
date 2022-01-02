00c9d20122b3be12d1d618ea169f6a3f2f4cfb10
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2022-01-02 11:19:48 +0000

    implemented async await

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ const request = require('request');
 const bot = new Telegraf(process.env['TOKEN'])
 
 // Start
-bot.start((ctx) => {
-	ctx.reply('Hey there! My name is Krafel.\nType /help to see a list of all my commands!')
+bot.start(async(ctx) => {
+	await ctx.reply('Hey there! My name is Krafel.\nType /help to see a list of all my commands!')
 })
 
 // Help
-bot.help((ctx) => {
-	ctx.replyWithMarkdown('*Krafel Command List*\n\nCurrent commands:\n-/start : Shows the introductory message.\n-/help : Shows a list of all commands(this message).\n-/hello : Says hello.\n-/bye : Says bye.\n-/say <message> : Says the given message.\n-/poll <question> | <option 1>, <option 2>, <option 3> : Makes a poll.\n-/dog : Shows a random dog picture.')
+bot.help(async(ctx) => {
+	await ctx.replyWithMarkdown('*Krafel Command List*\n\nCurrent commands:\n-/start : Shows the introductory message.\n-/help : Shows a list of all commands(this message).\n-/hello : Says hello.\n-/bye : Says bye.\n-/dog : Shows a random dog picture!\n-/say <message> : Says the given message!\n-/poll <question> | <option 1>, <option 2>, <option 3> : Makes a poll!')
 })
 
 // Hello
-bot.command('hello', (ctx) => {
-	ctx.reply('Hello!')
+bot.command('hello', async(ctx) => {
+	await ctx.reply('Hello!')
 })
 
 // Bye
-bot.command('bye', (ctx) => {
-	ctx.reply('Bye!')
+bot.command('bye', async(ctx) => {
+	await ctx.reply('Bye!')
 })
 
 // Dog
-bot.command('dog', (ctx) => {
+bot.command('dog', async(ctx) => {
 	const options = {
 		method: "GET",
 		url: "https://dog.ceo/api/breeds/image/random"
 	}
 
-	request(options, (error, response, body) => {
+	request(options, async(error, response, body) => {
 		photo = JSON.parse(response.body)['message']
-		ctx.replyWithMarkdown(`[Woof🐶](${photo})`)
+		await ctx.replyWithMarkdown(`[Woof🐶](${photo})`)
 	})
 })
 
 // Say
-bot.command('say', (ctx) => {
+bot.command('say', async(ctx) => {
 	let message = ctx.message['text']
 	if (message !== '/say') {
 		message = message.split('/say')
@@ -45,15 +45,15 @@ bot.command('say', (ctx) => {
 
 		let message_id = ctx.message['id']
 
-		ctx.deleteMessage(message_id)
-		ctx.reply(message)
+		await ctx.deleteMessage(message_id)
+		await ctx.reply(message)
 	} else {
-		ctx.replyWithMarkdown('You need to specify a message to say!\n\n*Example:*\n/say Hello!')
+		await ctx.replyWithMarkdown('You need to specify a message to say!\n\n*Example:*\n/say Hello!')
 	}
 })
 
 // Poll
-bot.command('poll', (ctx) => {
+bot.command('poll', async(ctx) => {
 	try {
 		let message = ctx.message['text']
 		let params = message.replace('/poll ', '')
@@ -61,9 +61,9 @@ bot.command('poll', (ctx) => {
 		let question = params[0]
 		let options = params[1]
 		options = options.split(',')
-		ctx.replyWithPoll(question, options, { is_anonymous: false})
+		await ctx.replyWithPoll(question, options, { is_anonymous: false})
 	} catch (e) {
-		ctx.replyWithMarkdown('You need to follow the command syntax correctly!\n\n*Example:*\n/poll This is question | Option One, Option 2, Option 3\n\n*You must:*\n-Seprate the question and the options with a "|"(vertical slash).\n-Seprate the options with a ","(comma).\n-Specify atleast 2 options.\n\nYou can have upto 10 options.')
+		await ctx.replyWithMarkdown('You need to follow the command syntax correctly!\n\n*Example:*\n/poll This is question | Option One, Option 2, Option 3\n\n*You must:*\n-Seprate the question and the options with a "|"(vertical slash).\n-Seprate the options with a ","(comma).\n-Specify atleast 2 options.\n\nYou can have upto 10 options.')
 	}
 })
 
